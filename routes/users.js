71c118a5133667be926e0a44c9b9de8195315424
Spider-boy19 <Chrisematt04@gmail.com>
2026-04-29 23:17:11 -0400

    Fixed issues for User List assignment

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,33 @@ const router = express.Router();
 
 // Fake database
 const users = [
-    { id: 0, name: "George" },
-    { id: 1, name: "Justyna" }
+    { id: 0, firstName: "George",  lastName:"Salayka", age:40, gender:"male"},
 ];
 
 // GET all users
-router.get('/', (req, res) => {
-    res.send(users);
+router.route('/').get( (req, res) => {
+    res.render('users');
+}).post((req,res)=>{
+    const firstName = req.body.firstName;
+    const age = parseInt(req.body.lastName);
+    const gender = req.body.gender;
+    const lastName = req.body.lastName;
+    const isValid = firstName !=="" && lastName !=="" && age!=="";
+    if (isValid){
+        console.log(`Adding user: ${firstName}`);
+        users.push({firstName, lastName, age, gender});
+        console.log("users", users);
+        res.render('users/list' ,{users});
+    }
+    else{
+        console.log("Error adding users!");
+        res.render("users/new", {firstName:firstName});
+    }
 });
 
+router.get(`list`, (req,res)=>{
+    res.render('users/list', {users});
+})
 // GET new user form
 router.get('/new', (req, res) => {
     res.render('users/new', { firstName: "Test" });
@@ -23,7 +41,7 @@ router.route('/:id')
     if (!req.user) {
         return res.status(404).send("User not found");
     }
-    res.send(`Getting User Data for: ${req.user.name}`);
+    res.render('users/user', {user:req.user}); //instead do res.render to a page and send back the whole user object
 })
 .delete((req, res) => {
     if (!req.user) {
@@ -40,13 +58,13 @@ router.route('/:id')
 
 // Middleware to get user by ID
 router.param("id", (req, res, next, id) => {
-    const user = users.find(u => u.id === parseInt(id));
-
-    if (!user) {
-        req.user = null;
-    } else {
-        req.user = user;
-    }
+    // const user = users.find(u => u.id === parseInt(id));
+    req.user=users[id];
+    // if (!user) {
+    //     req.user = null;
+    // } else {
+    //     req.user = user;
+    // }
 
     next();
 });
